3a8b009dcbb59fc1f3d58fde1362de00ed9623ee
SHAD RAYHAN MAZUMDER <shadmazumder@shads-macbook-pro.local>
2018-10-30 00:15:30 +0600

    Initiating LogTest and Logger.
    Added son file with data.

---
+++ DateCaseStudy.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+import XCTest
+
+struct Logger{
+    static let JsonFileName = "Log"
+}
+
+class LogTests: XCTestCase{
+    func testSetup() {
+        XCTAssert(true , "Things are not 👌")
+    }
+    
+    // MARK: - Json
+    func test_jsonFile_exits() {
+        XCTAssertNotNil(Bundle.main.path(forResource: Logger.JsonFileName, ofType: ".json"))
+    }
+}
+
+class TestObserver: NSObject, XCTestObservation {
+    func testCase(_ testCase: XCTestCase,
+                  didFailWithDescription description: String,
+                  inFile filePath: String?,
+                  atLine lineNumber: Int) {
+        assertionFailure(description, line: UInt(lineNumber))
+    }
+}
+let testObserver = TestObserver()
+XCTestObservationCenter.shared.addTestObserver(testObserver)
+LogTests.defaultTestSuite.run()
