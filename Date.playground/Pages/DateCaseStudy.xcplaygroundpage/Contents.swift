ab685f359475d0009ad38440978ba04397ed74a3
SHAD RAYHAN MAZUMDER <shadmazumder@shads-macbook-pro.local>
2018-11-06 23:06:42 +0600

    Updating the json file
    Adding test to parse the object.

--- Date.playground/Pages/DateCaseStudy.xcplaygroundpage/Contents.swift
+++ Date.playground/Pages/DateCaseStudy.xcplaygroundpage/Contents.swift
@@ -1,6 +1,34 @@
 import UIKit
 import XCTest
 
+// MARK: - OperatingTime
+struct OperatingTime: Decodable {
+    let start: Date
+    let end: Date
+}
+
+extension OperatingTime{
+    enum CodingKeys: String, CodingKey {
+        case start
+        case end
+    }
+}
+
+// MARK: - Operation
+struct Operation: Decodable{
+    let operatingTime: OperatingTime
+    let machineId: String
+    let plant: String
+}
+
+extension Operation{
+    enum CodingKeys: String, CodingKey {
+        case operatingTime = "operating_time"
+        case machineId = "machine_id"
+        case plant = "plant_id"
+    }
+}
+
 struct Logger{
     static let JsonFileName = "Log"
 }
@@ -16,12 +44,33 @@ class LogTests: XCTestCase{
     }
     
     func test_jsonFile_contains_jsonData() {
-        if let url = Bundle.main.url(forResource: Logger.JsonFileName, withExtension: "json") , let data = try? Data(contentsOf:url, options: .mappedIfSafe){
-            XCTAssertNotNil(try JSONSerialization.jsonObject(with: data, options: .mutableLeaves), "👎 json file contains no json-object")
-        }else{
-            XCTAssert(true, "👎 File not found")
+        guard let data = dataFromFile() else {
+            XCTAssert(false, "👎 no Data")
+            return
+        }
+        XCTAssertNotNil(try JSONSerialization.jsonObject(with: data, options: .mutableLeaves),
+                        "👎 json file contains no json-object")
+    }
+    
+    private func dataFromFile() -> Data? {
+        guard let url = Bundle.main.url(forResource: Logger.JsonFileName, withExtension: "json") , let data = try? Data(contentsOf:url, options: .mappedIfSafe) else { return nil }
+        return data
+    }
+    
+    // MARK: - Json to Custom types
+    func test_jsonData_converts_customType() {
+        guard let data = dataFromFile() else { return }
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        do {
+            let model = try decoder.decode([Operation].self, from: data)
+            print(model)
+        }
+        catch {
+            XCTAssert(false, "🤯 \(error)")
         }
     }
+    
 }
 
 class TestObserver: NSObject, XCTestObservation {
